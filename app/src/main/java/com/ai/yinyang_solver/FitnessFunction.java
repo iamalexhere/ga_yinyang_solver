add3ba35908f73a6150b594ede4298c3a40f2a19
6182201091 <6182201091@student.unpar.ac.id>
2024-12-11 17:08:32 +0700

    initialized dari git lain

--- app/src/main/java/com/ai/yinyang_solver/FitnessFunction.java
+++ app/src/main/java/com/ai/yinyang_solver/FitnessFunction.java
@@ -1,5 +1,6 @@
 package com.ai.yinyang_solver;
 
-public interface FitnessFunction {
+public interface FitnessFunction<C extends Chromosome<C>, T extends Comparable<T>> {
     
+    T calculate(C chromosome);
 }
